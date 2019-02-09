63a4a3969181fac15d73ebc5c540b2f02daf8893
Mika Hamalainen <mika.hamalainen@gmail.com>
2019-02-09 14:12:27 +0200

    Added react-router-dom for routing

---
+++ src/components/app_index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class AppIndex extends Component {
+  render() {
+    return (
+      <div>Hello, React starter!</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import AppIndex from './components/app_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={AppIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
